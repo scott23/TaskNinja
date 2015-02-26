e0aa5b3a064e662a486995b7c7b57b33a69d7c76
Scott Stiffler <scottstiffler@outlook.com>
2015-02-25 22:14:46 -0600

    Completed app from tutorial

--- scripts/app.js
+++ scripts/app.js
@@ -3,15 +3,57 @@
 var app = angular
   .module('TaskNinjaApp', [
     'ngAnimate',
-    'ngResource',    
-    'ngRoute',    
-    'firebase'
+    'ngResource',
+    'ngRoute',
+    'firebase',
+    'toaster',
+    'angularMoment'
   ])
-  .constant('FURL', 'https://your-firebase.firebaseio.com/')  
+  .constant('FURL', 'https://sms-taskninja.firebaseio.com/')
+
+  .run(function($location, $rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+        $rootScope.title = current.$$route.title;
+    });
+
+    $rootScope.$on('$routeChangeError', function(event, next, previous, error) {
+      if (error === "AUTH_REQUIRED") {
+        $location.path("/login");
+      }
+    });
+  })
+
   .config(function ($routeProvider) {
-    $routeProvider      
+    $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html'        
+        templateUrl: 'views/browse.html',
+        controller: 'BrowseController',
+        title: 'Browse'
+      })
+      .when('/browse/:taskId', {
+        templateUrl: 'views/browse.html',
+        controller: 'BrowseController',
+        title: 'Browse'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'AuthController',
+        title: 'Login'
+      })
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'AuthController',
+        title: 'Register'
+      })
+      .when('/dashboard', {
+        templateUrl: 'views/dashboard.html',
+        controller: 'DashboardController',
+        title: 'Dashboard',
+        resolve: {
+          currentAuth: function(Auth) {
+            return Auth.requireAuth();
+          }
+        }
       })
       .otherwise({
         redirectTo: '/'
